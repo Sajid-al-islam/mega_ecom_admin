c707b5bd6c5497297cbb9d07eab52c0c4ea20303
Md-shefat-masum <mshefat924@gmail.com>
2024-07-11 02:13:41 +0600

    manufacture

---
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Manufacture/setup/form_fields.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        name: "title",
+        label: "Title",
+        type: "text",
+        value: "",
+    },
+    {
+        name: "serial",
+        label: "serial",
+        type: "text",
+        value: "",
+    },
+
+    // {
+    //     name: "brand_discount",
+    //     label: "Brand Discount % ( 1 - 100 )",
+    //     type: "text",
+    //     value: "",
+    // },
+
+    {
+        name: "image",
+        label: "Image",
+        type: "file",
+        multiple: false,
+        value: "",
+    },
+
+
+];
---
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Manufacture/setup/index.js
@@ -0,0 +1,11 @@
+let setup = {
+    page_title: `Manufacture Management`,
+
+    route_prefix_name: `Manufactures`,
+    route_prefix: `manufactures`,
+
+    api_prefix: `manufactures`,
+
+    role_prefix:`admin`
+}
+export default setup;

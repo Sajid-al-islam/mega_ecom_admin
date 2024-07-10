bfb10d18ddfc11c4c162f97534d0d5b4e0b64efe
Md-shefat-masum <mshefat924@gmail.com>
2024-07-11 02:10:02 +0600

    text editor setup

--- resources/js/backend/views/components/TextEditor.vue
+++ resources/js/backend/views/components/TextEditor.vue
@@ -1,54 +1,127 @@
-<template>
+<template lang="">
     <div>
-        <div :id="name"></div>
+        <div>
+            <button type="button" @click="is_show = !is_show;" class="btn btn-info">Click Here to Modify</button>
+        </div>
+        <div class="editor_description_box" :class="{active: is_show}">
+            <div class="editor_box" :id="data_store">
+                <div :id="`editor`+data_store" v-html="data"></div>
+            </div>
+            <button type="button" @click="is_show = !is_show;" class="btn btn-info mb-3">Close</button>
+        </div>
     </div>
 </template>
-
 <script>
+
 export default {
     props: {
-        name: {
-            required: true,
+        default_data: {
             type: String,
+            default: '',
         },
+        data_store: String,
+        set_value: Function,
     },
-    created() {
-        this.initialiteSummerNote('description')
-        this.summerNoteTooltip('Style', 'dropdown-style')
-        this.summerNoteTooltip('Font Family', 'dropdown-fontname')
-        this.summerNoteTooltip('More Color', 'note-color')
-        this.summerNoteTooltip('Paragraph', 'note-color')
-        this.summerNoteTooltip('Table', 'note-table')
+    created: function () {
+        if (this.default_data) {
+            this.data = this.default_data;
+        }
 
-
+        // let that = this;
+        // setTimeout(() => {
+        //     that.editor = CKEDITOR.replace('editor');
+        // }, 500);
     },
+    mounted: function(){
+        this.editor = CKEDITOR.replace('editor'+this.data_store);
+    },
+    watch: {
+        'data': {
+            handler: function (v) {
+                this.set_value(this.data_store, v);
+            }
+        },
+        'editor': {
+            handler: function (v) {
+                v.on('change',this.set_data);
+            },
+        }
+    },
+    data: () => ({
+        data: '',
+        is_show: false,
+        editor: null,
+    }),
     methods: {
-        initialiteSummerNote(id) {
-            setTimeout(function () {
-                $(`#${id}`).summernote({
-                    height: 216,
-                    tabsize: 2,
-                });
-            }, 1000)
+        onEditorInput: function (data, event, editor) {
+            // console.log(data);
         },
-        summerNoteTooltip(style, classname) {
-            setTimeout(function () {
-                let target = document.querySelector(`[data-bs-original-title="${style}"]`);
-                let targetClass = document.querySelector(`.${classname}`);
-                target.addEventListener('click', function () {
-                    targetClass.classList.toggle('show');
-                    if (classname == 'note-color' || classname == 'note-table') {
-                        target.nextSibling.classList.toggle('show')
-                    }
-                });
-            }, 1000)
-        },
-    }
+        set_data: function(e){
+            this.data = e.editor.getData();
+            console.log(this.data);
+        }
+    },
 }
 </script>
-
-<style>
-.popover-content.note-children-container {
-    background: gray;
+<style scoped>
+.ck-editor__main {
+    max-height: calc(100vh - 215px);
+    overflow-y: auto;
 }
+
+.editor_description_box {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, .5);
+    z-index: 9999;
+    transform: scale(0);
+    opacity: 0;
+    visibility: hidden;
+    transition-duration: .3s;
+
+    .editor_box {
+        position: absolute;
+        top: calc(50% + 13px);
+        left: 50%;
+        height: calc(100% - 79px);
+        width: calc(100% - 20px);
+        overflow-y: auto;
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+
+    &.active {
+        transform: scale(1);
+        opacity: 1;
+        visibility: visible;
+    }
+
+}
+
+.list_card tbody tr td {
+    text-align: unset;
+}
+
+.ck-content .table {
+    float: left;
+}
+.cke_notifications_area{
+    display: none !important;
+}
+
+/* .ck-content .table table {
+        border-style: dashed;
+        border-color: hsl(90, 75%, 60%);
+        border-width: 3px;
+    } */
+
+/* .ck-content .table table td {
+        text-align: center;
+        vertical-align: middle;
+        padding: 10px
+    } */
 </style>

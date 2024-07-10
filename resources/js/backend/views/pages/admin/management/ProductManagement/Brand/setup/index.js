702310b405b1236b0c16a65f225573413cdae245
Md-shefat-masum <mshefat924@gmail.com>
2024-07-11 02:12:56 +0600

    brand manageme

---
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Brand/setup/index.js
@@ -0,0 +1,11 @@
+let setup = {
+    page_title: `Brand Management`,
+
+    route_prefix_name: `Brands`,
+    route_prefix: `brands`,
+
+    api_prefix: `brands`,
+
+    role_prefix:`admin`
+}
+export default setup;

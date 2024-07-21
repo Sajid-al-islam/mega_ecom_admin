fabe69d71f56bcfcc358a41e2f48f0510046e174
Md-shefat-masum <mshefat924@gmail.com>
2024-07-22 02:24:56 +0600

    header menu set

--- resources/js/backend/views/pages/admin/partials/TopHeader.vue
+++ resources/js/backend/views/pages/admin/partials/TopHeader.vue
@@ -9,29 +9,7 @@
             <div class="search-bar flex-grow-1"></div>
 
             <ul class="navbar-nav align-items-center right-nav-link ml-auto">
-                <li class="nav-item dropdown language">
-                    <a
-                        class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative"
-                        data-toggle="dropdown"
-                        href="javascript:void();"
-                        aria-expanded="false"
-                        ><i class="flag-icon flag-icon-gb align-middle"></i
-                    ></a>
-                    <ul class="dropdown-menu dropdown-menu-right">
-                        <li class="dropdown-item">
-                            <i class="flag-icon flag-icon-gb mr-3"></i>English
-                        </li>
-                        <li class="dropdown-item">
-                            <i class="flag-icon flag-icon-fr mr-3"></i>French
-                        </li>
-                        <li class="dropdown-item">
-                            <i class="flag-icon flag-icon-cn mr-3"></i>Chinese
-                        </li>
-                        <li class="dropdown-item">
-                            <i class="flag-icon flag-icon-de mr-3"></i>German
-                        </li>
-                    </ul>
-                </li>
+             
                 <li class="nav-item dropdown dropdown-lg">
                     <a
                         class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative"

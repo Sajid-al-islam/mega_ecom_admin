cbb2af45713643c553b1d05629e43db962694799
Md-shefat-masum <mshefat924@gmail.com>
2024-07-22 02:21:15 +0600

    data create

--- resources/js/backend/views/pages/admin/management/UserManagement/users/components/all_data_page/action_buttons/Create.vue
+++ resources/js/backend/views/pages/admin/management/UserManagement/users/components/all_data_page/action_buttons/Create.vue
@@ -1,12 +1,17 @@
 <template lang="">
-    <a href="" class="btn action_btn mr-2 btn-sm btn-info d-flex align-content-center align-items-center">
+    <router-link
+        :to="{name: `Create${setup.route_prefix}`}"
+        class="btn action_btn mr-2 btn-sm btn-info d-flex align-content-center align-items-center">
         <i class="fa fa-edit mr-2"></i>
         Create
-    </a>
+    </router-link>
 </template>
 <script>
+import setup from "../../../setup"
 export default {
-
+    data: ()=>({
+        setup,
+    })
 }
 </script>
 <style lang="">

9d0095e98461554f3c759eed6227a35b944d2a49
Md-shefat-masum <mshefat924@gmail.com>
2024-07-22 20:34:49 +0600

    product management init

--- resources/js/backend/views/pages/admin/management/ProductManagement/Brand/Form.vue
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Brand/Form.vue
@@ -1,34 +1,37 @@
 <template>
-    <div class="container">
-
+    <div>
         <form @submit.prevent="submitHandler">
             <div class="card">
                 <div class="card-header d-flex justify-content-between">
                     <h5 class="text-capitalize">{{ param_id ? 'Update' : 'Create' }} new {{ route_prefix }}</h5>
                     <div>
-                        <router-link class="btn btn-outline-warning btn-sm" :to="{ name: `All${route_prefix_name}` }">
+                        <router-link class="btn btn-outline-info mr-2 btn-sm"
+                            :to="{ name: `Details${route_prefix}`, params: {id: item.slug} }">
+                            Details {{ route_prefix }}
+                        </router-link>
+                        <router-link class="btn btn-outline-warning btn-sm" :to="{ name: `All${route_prefix}` }">
                             All {{ route_prefix }}
                         </router-link>
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="card-body card_body_fixed_height">
                     <div class="row">
-                        <div class="col-xl-7">
-                            <div class="row">
-                                <div class="col-md-12" v-for="(form_field, index) in form_fields" :key="index">
-                                    <common-input :label="form_field.label" :type="form_field.type"
-                                        :name="form_field.name" :multiple="form_field.multiple"
-                                        :value="form_field.value" :data_list="form_field.data_list" />
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-light btn-square px-5">
-                                    <i class="icon-login"></i>
-                                    Submit
-                                </button>
-                            </div>
+                        <div class="col-md-6" v-for="(form_field, index) in form_fields" :key="index">
+                            <common-input
+                                :label="form_field.label"
+                                :type="form_field.type"
+                                :name="form_field.name"
+                                :multiple="form_field.multiple"
+                                :value="form_field.value"
+                                :data_list="form_field.data_list" />
                         </div>
                     </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-light btn-square px-5">
+                        <i class="icon-lock"></i>
+                        Submit
+                    </button>
                 </div>
             </div>
         </form>
@@ -37,80 +40,73 @@
 
 <script>
 import { mapActions, mapState } from 'pinia'
-import { user_setup_store } from './setup/store';
+import { store } from './setup/store';
 import setup from "./setup";
 import form_fields from "./setup/form_fields";
 
 export default {
     data: () => ({
-        route_prefix: setup.route_prefix,
-        route_prefix_name: setup.route_prefix_name,
-
+        route_prefix: '',
         form_fields,
         param_id: null,
     }),
     created: async function () {
-        let id = this.$route.query.id;
-        this.form_fields.forEach((item) => {
-            item.value = "";
-        });
+        let id = this.param_id = this.$route.params.id;
+        this.route_prefix = setup.route_prefix;
 
-        return;
-        await this.get_all_data()
+        this.reset_fields();
+
         if (id) {
-            this.param_id = id;
-            await this.get_single_data(id);
-            if (this.single_data) {
-                this.form_fields.forEach((field, index) => {
-                    Object.entries(this.single_data).forEach((value) => {
-                        if (field.name == value[0]) {
-                            this.form_fields[index].value = value[1];
-                        }
-
-
-                    });
-                });
-            }
-        } else {
-            this.form_fields.forEach((item) => {
-                item.value = "";
-            });
+            this.set_fields(id);
         }
     },
     methods: {
-        ...mapActions(user_setup_store, {
-            get_all_data: 'all',
-            get_single_data: 'get',
-            store_data: 'store',
-            update_data: 'update',
+        ...mapActions(store, {
+            create: 'create',
+            update: 'update',
+            details: 'details',
         }),
+        reset_fields: function () {
+            this.form_fields.forEach((item) => {
+                item.value = "";
+            });
+        },
+        set_fields: async function (id) {
+            this.param_id = id;
+            await this.details(id);
+            if (this.item) {
+                this.form_fields.forEach((field, index) => {
+                    Object.entries(this.item).forEach((value) => {
+                        if (field.name == value[0]) {
+                            this.form_fields[index].value = value[1];
+                        }
+                    });
+                });
+            }
+        },
 
         submitHandler: async function ($event) {
             if (this.param_id) {
-                let response = await this.update_data($event.target, this.param_id);
-                if (response.data.status === "success") {
-                    window.s_alert(response.data.message);
-                    this.$router.push({ name: `All${this.route_prefix}` });
+                let response = await this.update($event);
+                console.log(response);
+                if (response.status === 200) {
+                    window.s_alert("data updated");
+                    this.$router.push({ name: `Details${this.route_prefix}` });
                 }
             } else {
-                let response = await this.store_data($event.target);
-                if (response.data.status === "success") {
-                    window.s_alert(response.data.message);
+                let response = await this.create($event);
+                if (response.status === 200) {
+                    window.s_alert("data created");
                     this.$router.push({ name: `All${this.route_prefix}` });
                 }
             }
         },
-
-
     },
 
     computed: {
-        ...mapState(user_setup_store, {
-            single_data: "single_data",
-            all_data: 'all_data',
+        ...mapState(store, {
+            item: "item",
         }),
     },
-
-
 }
 </script>
--- resources/js/backend/views/pages/admin/management/ProductManagement/Brand/setup/form_fields.js
+++ resources/js/backend/views/pages/admin/management/ProductManagement/Brand/setup/form_fields.js
@@ -1,31 +1,76 @@
 export default [
     {
-        name: "title",
-        label: "Brand Title",
-        type: "text",
-        value: "",
-    },
-    {
-        name: "serial",
-        label: "Brand serial",
+        name: "name",
+        label: "name",
         type: "text",
         value: "",
     },
 
-    // {
-    //     name: "brand_discount",
-    //     label: "Brand Discount % ( 1 - 100 )",
-    //     type: "text",
-    //     value: "",
-    // },
+    {
+        name: "email",
+        label: "email",
+        type: "text",
+        value: "",
+    },
+    {
+        name: "password",
+        label: "password",
+        type: "text",
+        value: "",
+    },
 
     {
-        name: "image",
-        label: "Brand Image",
+        name: "phone_number",
+        label: "phone",
+        type: "text",
+        value: "",
+    },
+
+    {
+        name: "role_serial",
+        label: "User Role",
+        type: "select",
+        data_list: [
+            {
+                label: 'admin',
+                value: 2,
+            },
+            {
+                label: 'customer',
+                value: 3,
+            },
+            {
+                label: 'sales',
+                value: 4,
+            },
+            {
+                label: 'account',
+                value: 5,
+            },
+            {
+                label: 'retailer',
+                value: 6,
+            },
+            {
+                label: 'supplier',
+                value: 7,
+            },
+            {
+                label: 'delivary man',
+                value: 8,
+            },
+            {
+                label: 'employee',
+                value: 9,
+            },
+        ],
+    },
+
+    {
+        name: "photo",
+        label: "photo",
         type: "file",
         multiple: false,
         value: "",
     },
-
-
 ];

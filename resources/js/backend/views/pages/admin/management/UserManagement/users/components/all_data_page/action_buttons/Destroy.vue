4dd7f7cdbac6a2ecc9eb2b3ce76d451c1af7c8fa
Md-shefat-masum <mshefat924@gmail.com>
2024-07-22 20:33:47 +0600

    destroy button dynamic

--- resources/js/backend/views/pages/admin/management/UserManagement/users/components/all_data_page/action_buttons/Destroy.vue
+++ resources/js/backend/views/pages/admin/management/UserManagement/users/components/all_data_page/action_buttons/Destroy.vue
@@ -1,12 +1,41 @@
 <template lang="">
-    <a href="/users/destroy/2" class="border-danger">
+    <a href="/destroy" @click.prevent="destroy_data" class="border-danger">
         <i class="fa fa-trash text-danger"></i>
         Destroy
     </a>
 </template>
 <script>
-export default {
+import { mapActions } from 'pinia';
+import { store } from '../../../setup/store';
 
+export default {
+    props: {
+        item: {
+            slug: "",
+        }
+    },
+    methods: {
+        ...mapActions(store, [
+            `restore`,
+            `destroy`,
+            `get_all`,
+            `set_only_latest_data`,
+            `set_item`,
+        ]),
+        destroy_data: async function(){
+            let con = await window.s_confirm('Permanently delete');
+            if(con){
+                this.set_item(this.item);
+                this.set_only_latest_data(true);
+
+                await this.destroy();
+                await this.get_all();
+                window.s_alert('Permanently deleted.');
+
+                this.set_only_latest_data(false);
+            }
+        },
+    }
 }
 </script>
 <style lang="">
